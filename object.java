e1f0f2487f0907ec4593b1ee14033827143e182f
The Tech Coder <91053240+The-Tech-Coder@users.noreply.github.com>
2021-11-28 22:35:52 +0530

    Create object.java

---
+++ object.java
@@ -0,0 +1,11 @@
+public class CreateObject  {    
+    void show(){    
+        System.out.println("Object Creation using new keyword.");    
+    }    
+    public static void main(String[] args){    
+        //creating an object using new keyword   
+        CreateObject obj = new CreateObject();   
+        //invoking method using the object  
+        obj.show();    
+    }    
+}   
