57d5cc53489558b0f7820391d69254bad422b3e2
The Tech Coder <91053240+The-Tech-Coder@users.noreply.github.com>
2021-12-05 22:12:57 +0530

    Create encapsulation2.java

---
+++ encapsulation2.java
@@ -0,0 +1,15 @@
+//save this file as "myName.java"
+
+package Encapsulation;
+public class myName{  
+    //private data member  
+    private String name;  
+    //getter method for name  
+    public String getName(){  
+        return name;  
+    }  
+    //setter method for name  
+    public void setName(String name){  
+        this.name=name;  
+    }  
+}  
