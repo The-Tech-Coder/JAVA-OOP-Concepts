fd59226dd634dc83954f6656e86e2eb10cd7daa9
The Tech Coder <91053240+The-Tech-Coder@users.noreply.github.com>
2021-12-05 22:11:31 +0530

    Create encapsulation.java

---
+++ encapsulation.java
@@ -0,0 +1,14 @@
+//save this file as "Detail.java"
+
+//A Java class to test the encapsulated class.  
+package Encapsulation;  
+class Detail{  
+    public static void main(String[] args){  
+        //creating instance of the encapsulated class  
+        myName a=new myName();  
+        //setting value in the name member  
+        a.setName("Lucia");  
+        //getting value of the name member  
+        System.out.println(a.getName());  
+    }  
+}  
